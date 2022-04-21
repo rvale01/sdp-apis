262c7620b00474ddbe34b4bd20f5b02a18274459
am2-feetham <alexander2.feetham@live.uwe.ac.uk>
2022-04-21 15:34:42 +0100

    Implemented create user, get patients, edit patient and created gitignore for pycache

--- auth.py
+++ auth.py
@@ -22,3 +22,89 @@ def login():
             return "error connecting to db"
     else:
         return "error connecting to db"
+
+def create_user(email, hpasswd, full_name, role):
+    conn = database.get_connection()
+    if conn != None:
+        dbcursor = conn.cursor()
+        query = """INSERT INTO Users
+            VALUES (%s, %s, %s, %s);"""
+        data = (email, hpasswd, full_name, role)
+        dbcursor.execute(query, data)
+        conn.commit()
+        dbcursor.close()
+        conn.close()
+        return True
+    return False
+
+def get_patients(email):
+    conn = database.get_connection()
+    if conn != None:
+        dbcursor = conn.cursor()
+        query = f"""
+            SELECT * FROM patients
+            INNER JOIN patient_staff ON patients.patient_id=patient_staff.patient_id
+            WHERE patient_staff.staff_email='{email}'"""
+        dbcursor.execute(query)
+        data = dbcursor.fetchall()
+        dbcursor.close()
+        conn.close()
+        return data
+    return None
+
+def edit_patient(patient_id,
+            full_name=None,
+            admission_date=None,
+            estimated_leave_date=None,
+            medication_needed=None,
+            last_medicated=None,
+            allergies=None,
+            awaiting_tests=None,
+            test_reviewed=None,
+            emergency_contact=None,
+            resuscitation_preference=None,
+            referred=None,
+            csv_data=None):
+    args = ""
+    vars = locals()
+    for i in vars:
+        if vars[i] != None:
+            if(str(i) == "csv_data"):
+                args += data_to_columns(vars[i])
+            else:
+                args += str(i) + "=" + "'" + str(vars[i]) + "', "
+    conn = database.get_connection()
+    if conn != None:
+        dbcursor = conn.cursor()
+        query = f"""
+            UPDATE patients
+            SET {args[args.find(",") + 2:-11]}
+            WHERE patient_id = '{patient_id}';
+        """
+        dbcursor.execute(query)
+        conn.commit()
+        dbcursor.close()
+        conn.close()
+
+def data_to_columns(csv_data):
+    #Assuming data is complete
+    names = ["end_tidal_co2",
+    "feed_vol",
+    "feed_vol_adm",
+    "fio2",
+    "fio2_ratio",
+    "insp_time",
+    "oxygen_flow_rate",
+    "peep",
+    "pip",
+    "resp_rate",
+    "sip",
+    "tidal_vol",
+    "tidal_vol_actual",
+    "tidal_vol_kg",
+    "tidal_vol_spon",
+    "bmi"]
+    output = ""
+    for i, e in zip(csv_data, names):
+        output += str(e) + " = '" + i + "', "
+    return output
